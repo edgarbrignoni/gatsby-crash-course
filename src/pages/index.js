c2a78ffe19e8e00cfda3d987cf0eae6bdee2846a
Edgar Brignoni <edgarbrignoni@icloud.com>
2021-05-20 11:35:07 -0400

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Welcome to my website</h1>
+    <h1>Welcome to My Awesome Website</h1>
     <p>This is a sample site for the Gatsby crash course</p>
   </div>
 )
